f26263a9705c96276796c2bd1ac8f560f4ee29c3
Teddy Rodd <99229169+Morbanaa@users.noreply.github.com>
2026-05-04 09:08:15 -0500

    Refactor input prompts to include entry numbers. Placed loops inside main loop with option to quit each cycle

--- main.py
+++ main.py
@@ -8,24 +8,33 @@ def main():
     department = ""
     job_title = ""
 
+    counter = 0
     while True:
-        name = input("Enter your name: ").upper()
-        if name != "" and name != " ":
-            break
+        counter +=1
+        while True:
+            name = input(f"Enter name #{counter}: ").upper()
+            if name != "" and name != " ":
+                break
 
-    while True:
-        id_number = input("Enter your id number: ").upper()
-        if id_number != "" and id_number != " ":
-            break
+        while True:
+            id_number = input(f"Enter id number #{counter}: ").upper()
+            if id_number != "" and id_number != " ":
+                break
 
-    while True:
-        department = input("Enter your department: ").upper()
-        if department != "" and department != " ":
-            break
+        while True:
+            department = input(f"Enter department #{counter}: ").upper()
+            if department != "" and department != " ":
+                break
 
-    while True:
-        job_title = input("Enter your job title: ").upper()
-        if job_title != "" and job_title != " ":
+        while True:
+            job_title = input(f"Enter job title #{counter}: ").upper()
+            if job_title != "" and job_title != " ":
+                break
+
+        choice = input("Would you like to make another entry?(Y/N)" ).upper()
+        if choice == "Y" or choice == "YES":
+            continue
+        else:
             break
 
 
