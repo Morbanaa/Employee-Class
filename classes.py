75c93b60cd7f80634e002a0f3fc28a54e6e7e02c
Teddy Rodd <99229169+Morbanaa@users.noreply.github.com>
2026-05-04 08:59:57 -0500

    Add Employee class with initialization and display method

--- classes.py
+++ classes.py
@@ -1,1 +1,9 @@
+class Employee():
+    def __init__(self,name,id_number,department,job_title):
+        self.name = name
+        self.id_number = id_number
+        self.department = department
+        self.job_title = job_title
 
+    def display(self):
+        print(f"{self.name} {self.id_number} {self.department} {self.job_title}")
