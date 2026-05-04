d9fa373fde229996e82514ac42a791573546562a
Teddy Rodd <99229169+Morbanaa@users.noreply.github.com>
2026-05-04 09:01:59 -0500

    Implement employee input collection in main function
    Add input prompts for employee details in main function

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+from classes import Employee
+
+def main():
+    employees = []
+
+    name = ""
+    id_number = ""
+    department = ""
+    job_title = ""
+
+    while True:
+        name = input("Enter your name: ").upper()
+        if name != "" and name != " ":
+            break
+
+    while True:
+        id_number = input("Enter your id number: ").upper()
+        if id_number != "" and id_number != " ":
+            break
+
+    while True:
+        department = input("Enter your department: ").upper()
+        if department != "" and department != " ":
+            break
+
+    while True:
+        job_title = input("Enter your job title: ").upper()
+        if job_title != "" and job_title != " ":
+            break
+
+
+if __name__ == "__main__":
+    main()
 
